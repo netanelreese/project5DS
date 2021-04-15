b997c924540bb855b735451f1aa08078d09c72eb
Nathanael <ngreese7@ou.edu>
2021-04-15 10:31:58 -0500

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/project5_2413.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project5_2413.dir/main.cpp.obj"
+  "project5_2413.exe"
+  "project5_2413.exe.manifest"
+  "project5_2413.lib"
+  "project5_2413.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project5_2413.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/project5_2413.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for project5_2413.
+# This may be replaced when dependencies are built.
